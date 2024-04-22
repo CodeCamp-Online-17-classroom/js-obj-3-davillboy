c0f2022654bd1385824ccfeb73148152474b3764
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 14:47:21 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // write code here
+<script>
+        let user = {
+            name: "John",
+            surname: "Doe"
+          };
+          
+          user.name = "Matt";
+          delete user.surname;
+          
+          console.log(user); 
+          
+    </script>
